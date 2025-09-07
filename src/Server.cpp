20c3ded80091640191ed159a5c0030d0d674d7fe
Hardaat Singh Baath <hardaatbaath11@gmail.com>
2025-09-07 15:24:21 +0530

    End of String Anchor: Imporved the logic for this

--- src/Server.cpp
+++ src/Server.cpp
@@ -78,7 +78,7 @@ bool match_string(const std::string &input_line, const std::string &pattern) {
             input_pos++;
             pattern_pos++; // I am better than Claude
         }
-        if (pattern.at(pattern_pos) == '$') return true;
+        if (pattern.at(pattern_pos) == '$' && input_pos == input_length && match_found) return true;
 
         // If we matched the entire pattern, return true
         if (match_found && pattern_pos == pattern_length) return true;
